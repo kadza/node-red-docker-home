205ad59044291fe18241252ce9bc5b73bd037980
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-11-20 09:54:28 +0000

    Single platform build works

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
+source .env
 export NODE_RED_VERSION=$(grep -oE "\"node-red\": \"(\w*.\w*.\w*.\w*.\w*.)" package.json | cut -d\" -f4)
 
 echo "#########################################################################"
 echo "node-red version: ${NODE_RED_VERSION}"
 echo "#########################################################################"
 
-docker build --rm --no-cache \
+docker buildx build \
     --build-arg ARCH=amd64 \
+    --build-arg NPM_TOKEN=${NPM_TOKEN} \
     --build-arg NODE_VERSION=16 \
     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
     --build-arg OS=alpine \
     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
-    --build-arg TAG_SUFFIX=default \
+    --build-arg TAG_SUFFIX=minimal \
+    --network=host \
     --file Dockerfile.custom \
+    --platform linux/amd64 \
     --tag testing:node-red-build .
+
+# docker build --rm --no-cache \
+#     --build-arg ARCH=arm64v8 \
+#     --build-arg NPM_TOKEN=${NPM_TOKEN} \
+#     --build-arg NODE_VERSION=16 \
+#     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
+#     --build-arg OS=alpine \
+#     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
+#     --build-arg TAG_SUFFIX=minimal \
+#     --network=host \
+#     --file Dockerfile.custom \
+#     --tag testing:node-red-build .
