4687e40b718b23e7f07db87bec76f610b998e09f
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-11-20 10:42:48 +0000

    Seems to be it, testing remains

--- docker-custom/docker-alpine.sh
+++ docker-custom/docker-alpine.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source .env
+TAG="${1:-latest}"
 export NODE_RED_VERSION=$(grep -oE "\"node-red\": \"(\w*.\w*.\w*.\w*.\w*.)" package.json | cut -d\" -f4)
 
 echo "#########################################################################"
@@ -16,17 +17,6 @@ docker buildx build \
     --build-arg TAG_SUFFIX=minimal \
     --network=host \
     --file Dockerfile.custom \
-    --platform linux/amd64 \
-    --tag testing:node-red-build .
-
-# docker build --rm --no-cache \
-#     --build-arg ARCH=arm64v8 \
-#     --build-arg NPM_TOKEN=${NPM_TOKEN} \
-#     --build-arg NODE_VERSION=16 \
-#     --build-arg NODE_RED_VERSION=${NODE_RED_VERSION} \
-#     --build-arg OS=alpine \
-#     --build-arg BUILD_DATE="$(date +"%Y-%m-%dT%H:%M:%SZ")" \
-#     --build-arg TAG_SUFFIX=minimal \
-#     --network=host \
-#     --file Dockerfile.custom \
-#     --tag testing:node-red-build .
+    --platform linux/amd64,linux/arm64v8 \
+    --push \
+    --tag kadzaa/private:${TAG} .
